1368b1cd378c058cadab816aa11f5d038ad0b377
Yogesh Aggarwal <developeryogeshgit@gmail.com>
2020-07-02 18:58:58 +0530

    Added page transitions

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:falcon/screens/chats/chats.dart';
-import 'package:falcon/widgets/bottomBar.dart';
+import 'package:falcon/services/data.service.dart';
 import 'package:falcon/widgets/navbar.dart';
 import 'package:falcon/widgets/stories/stories.dart';
 import 'package:flutter/material.dart';
@@ -8,32 +8,129 @@ void main() {
   runApp(Falcon());
 }
 
-class Falcon extends StatelessWidget {
+class Falcon extends StatefulWidget {
+  @override
+  _FalconState createState() => _FalconState();
+}
+
+class _FalconState extends State<Falcon> {
+  int navCurrentIndex = 2;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       builder: (context, child) {
         return ScrollConfiguration(
-          behavior: ScrollBehavior()..buildViewportChrome(context, null, AxisDirection.down),
+          behavior: ScrollBehavior()
+            ..buildViewportChrome(context, null, AxisDirection.down),
           child: child,
         );
       },
       debugShowCheckedModeBanner: false,
       home: SafeArea(
         child: Scaffold(
-          body: Container(
-            child: Column(
-              children: <Widget>[
-                Navbar(),
-                Stories(),
-                Divider(
-                  height: 40,
+          body: PageView(
+            children: <Widget>[
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    Stories(),
+                    Divider(height: 40),
+                    Chats(),
+                  ],
                 ),
-                Chats(),
-              ],
-            ),
+              ),
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    Navbar(),
+                    Stories(),
+                    Divider(height: 40),
+                    Chats(),
+                  ],
+                ),
+              ),
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    Navbar(),
+                    Stories(),
+                    Divider(height: 40),
+                    Chats(),
+                  ],
+                ),
+              ),
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    Navbar(),
+                    Stories(),
+                    Divider(height: 40),
+                    Chats(),
+                  ],
+                ),
+              ),
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    Navbar(),
+                    Stories(),
+                    Divider(height: 40),
+                    Chats(),
+                  ],
+                ),
+              ),
+            ],
+            controller: Data.pageController,
+            onPageChanged: (index) {
+              setState(() {
+                this.navCurrentIndex = index;
+              });
+            },
           ),
-          bottomNavigationBar: BottomBar(),
+          bottomNavigationBar: BottomNavigationBar(
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.people_outline, color: Colors.green[800]),
+                title: Text(""),
+                activeIcon: Icon(Icons.people, color: Colors.green[800]),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.label_outline, color: Colors.green[800]),
+                title: Text(""),
+                activeIcon: Icon(Icons.label, color: Colors.green[800]),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.chat_bubble_outline, color: Colors.green[800]),
+                title: Text(""),
+                activeIcon: Icon(Icons.chat_bubble, color: Colors.green[800]),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.outlined_flag, color: Colors.green[800]),
+                title: Text(""),
+                activeIcon: Icon(Icons.flag, color: Colors.green[800]),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person_outline, color: Colors.green[800]),
+                title: Text(""),
+                activeIcon: Icon(Icons.person, color: Colors.green[800]),
+              ),
+            ],
+            currentIndex: this.navCurrentIndex,
+            elevation: 0,
+            type: BottomNavigationBarType.fixed,
+            onTap: (index) {
+              setState(() {
+                this.navCurrentIndex = index;
+
+                Data.pageController.animateToPage(
+                  index,
+                  duration: Duration(seconds: 1),
+                  curve: Curves.easeInOutQuad,
+                );
+              });
+            },
+          ),
         ),
       ),
     );
---
+++ lib/services/data.service.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class Data {
+  static PageController pageController = new PageController(initialPage: 2);
+}
