c1a222b6823c9243421d7375e9b47ae76595eee6
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-09-09 13:22:37 +0530

    Seperated main thread

--- lib/main.dart
+++ lib/main.dart
@@ -2,62 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 
 void main() {
-  runApp(MemoMessenger());
+  runApp(Main());
 }
 
-class MemoMessenger extends StatefulWidget {
-  @override
-  _MemoMessengerState createState() => _MemoMessengerState();
-}
-
-class _MemoMessengerState extends State<MemoMessenger> {
-  int navCurrentIndex = 2;
-  List<Widget> pages = [];
-  int pageAnimationDuration = 200;
-  bool _visible = true;
-  PageController pageController = new PageController(initialPage: 2);
-
-  void prepareData() async {
-    // Data.getUser();
-    // Data.getChats();
-    // Data.getContacts();
-    // Data.getCalls();
-  }
-
-  @override
-  void initState() {
-    this.prepareData();
-    super.initState();
-  }
-
-  void animate() async {
-    setState(() {
-      _visible = false;
-    });
-    Future.delayed(Duration(milliseconds: this.pageAnimationDuration))
-        .then((_) {
-      setState(() {
-        _visible = true;
-      });
-    });
-  }
-
-  void changePage(int index) {
-    this.pageController.jumpToPage(index);
-  }
-
+class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
         accentColor: Colors.green,
         accentColorBrightness: Brightness.light,
-        buttonTheme: ButtonThemeData(
-          highlightColor: Colors.red,
-          hoverColor: Colors.red,
-          buttonColor: Colors.red,
-          splashColor: Colors.red,
-        ),
+        fontFamily: "Poppins",
+        textTheme: TextTheme(),
       ),
       builder: (context, child) {
         return ScrollConfiguration(
@@ -67,66 +23,83 @@ class _MemoMessengerState extends State<MemoMessenger> {
         );
       },
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: SafeArea(
-          child: PageView(
-            controller: pageController,
-            onPageChanged: (index) {
-              setState(() {
-                this.navCurrentIndex = index;
-              });
-            },
-            children: [
-              Container(),
-              Container(),
-              Container(),
-              Container(),
-              Container(),
-            ],
-          ),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.people_outline, color: Colors.green[800]),
-              title: Text("People"),
-              activeIcon: Icon(Icons.people, color: Colors.green[800]),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.label_outline, color: Colors.green[800]),
-              title: Text("Labels"),
-              activeIcon: Icon(Icons.label, color: Colors.green[800]),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.chat_bubble_outline, color: Colors.green[800]),
-              title: Text("Chats"),
-              activeIcon: Icon(Icons.chat_bubble, color: Colors.green[800]),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(LineAwesomeIcons.phone, color: Colors.green[800]),
-              title: Text("Call"),
-              activeIcon:
-                  Icon(Icons.phone, color: Colors.green[800]),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person_outline, color: Colors.green[800]),
-              title: Text("Profile"),
-              activeIcon: Icon(Icons.person, color: Colors.green[800]),
-            ),
-          ],
-          currentIndex: this.navCurrentIndex,
-          elevation: 20,
-          onTap: (index) {
-            if (this.navCurrentIndex != index) {
-              animate();
-              Future.delayed(
-                Duration(milliseconds: this.pageAnimationDuration),
-              ).then((_) {
-                this.changePage(index);
-              });
-            }
+      home: MemoMessenger(),
+    );
+  }
+}
+
+class MemoMessenger extends StatefulWidget {
+  @override
+  _MemoMessengerState createState() => _MemoMessengerState();
+}
+
+class _MemoMessengerState extends State<MemoMessenger> {
+  int _navCurrentIndex = 2;
+  PageController pageController = new PageController(initialPage: 2);
+
+  void prepareData() async {}
+
+  @override
+  void initState() {
+    this.prepareData();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: PageView(
+          controller: pageController,
+          onPageChanged: (int index) {
+            setState(() {
+              this._navCurrentIndex = index;
+            });
           },
+          children: [
+            Container(),
+            Container(
+              child: Text("Hello world!"),
+            ),
+            Container(),
+            Container(),
+            Container(),
+          ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people_outline, color: Colors.green[800]),
+            title: Text("People"),
+            activeIcon: Icon(Icons.people, color: Colors.green[800]),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.label_outline, color: Colors.green[800]),
+            title: Text("Labels"),
+            activeIcon: Icon(Icons.label, color: Colors.green[800]),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat_bubble_outline, color: Colors.green[800]),
+            title: Text("Chats"),
+            activeIcon: Icon(Icons.chat_bubble, color: Colors.green[800]),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(LineAwesomeIcons.phone, color: Colors.green[800]),
+            title: Text("Call"),
+            activeIcon: Icon(Icons.phone, color: Colors.green[800]),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline, color: Colors.green[800]),
+            title: Text("Profile"),
+            activeIcon: Icon(Icons.person, color: Colors.green[800]),
+          ),
+        ],
+        currentIndex: this._navCurrentIndex,
+        elevation: 20,
+        onTap: (index) {
+          this.pageController.jumpToPage(index);
+        },
       ),
     );
   }
