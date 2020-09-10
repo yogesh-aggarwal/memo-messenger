39d73253d186fdc95aaee444d7cbb6bab1320b0c
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-09-10 11:34:07 +0530

    Arranged UI files of chat screen

--- lib/Activities/ChatActivity/Activity.dart
+++ lib/Activities/ChatActivity/Activity.dart
@@ -1,130 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:line_awesome_flutter/line_awesome_flutter.dart';
+import 'package:memomessenger/Activities/ChatActivity/Messages.dart';
+import 'package:memomessenger/Activities/ChatActivity/WriteMessage.dart';
 import 'package:memomessenger/Services/Chats.dart';
-import 'package:memomessenger/Services/Constants.dart';
-import 'package:memomessenger/Services/Types/Chat.dart';
 import 'package:memomessenger/Services/Types/ChatsActivity.dart';
-import 'package:memomessenger/Services/User.dart';
-
-Widget buildMessageWidget(
-  BuildContext context,
-  Message message,
-  String currentUserId,
-) {
-  final bool isFromCurrentUser = message.senderId == currentUserId;
-  return Align(
-    alignment: isFromCurrentUser ? Alignment.centerRight : Alignment.centerLeft,
-    child: Container(
-      margin: EdgeInsets.symmetric(vertical: 3),
-      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-      constraints: BoxConstraints(
-        maxWidth: MediaQuery.of(context).size.width * .75,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.only(
-          topRight: Radius.circular(
-            isFromCurrentUser ? 0 : messageBorderRadius,
-          ),
-          topLeft: Radius.circular(
-            isFromCurrentUser ? messageBorderRadius : 0,
-          ),
-          bottomLeft: Radius.circular(messageBorderRadius),
-          bottomRight: Radius.circular(messageBorderRadius),
-        ),
-        boxShadow: isFromCurrentUser
-            ? [
-                BoxShadow(color: themeAccentColor[300], blurRadius: 10),
-              ]
-            : [
-                BoxShadow(color: Colors.black26, blurRadius: 10),
-              ],
-        color: isFromCurrentUser ? themeAccentColor[500] : Color(0xFFEbE7EA),
-      ),
-      child: Column(
-        children: [
-          Text(
-            message.text,
-            style: TextStyle(
-              color: isFromCurrentUser ? Colors.white : Colors.black,
-            ),
-          ),
-        ],
-      ),
-    ),
-  );
-}
-
-Widget buildChatMessagesUI(BuildContext context, List<Message> messages) {
-  final String currentUserId = currentUser.value.id;
-  return SingleChildScrollView(
-    reverse: true,
-    padding: EdgeInsets.symmetric(vertical: 10),
-    child: Container(
-      padding: EdgeInsets.symmetric(horizontal: 10),
-      child: Column(
-        verticalDirection: VerticalDirection.up,
-        children: messages.reversed.map((Message message) {
-          return buildMessageWidget(context, message, currentUserId);
-        }).toList(),
-      ),
-    ),
-  );
-}
-
-Widget buildMessageAction({@required IconData icon, @required Function onTap}) {
-  return GestureDetector(
-    onTap: onTap,
-    child: Container(
-      decoration: BoxDecoration(borderRadius: BorderRadius.circular(40)),
-      padding: EdgeInsets.symmetric(vertical: 6, horizontal: 6),
-      child: Icon(icon, color: themeAccentColor[800]),
-    ),
-  );
-}
-
-Widget buildMessageInputUI(BuildContext context, {@required String chatId}) {
-  final String currentUserId = currentUser.value.id;
-  String text = "";
-  return Container(
-    margin: EdgeInsets.only(top: 5),
-    padding: EdgeInsets.only(top: 2, bottom: 2, left: 5, right: 2),
-    child: Row(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        buildMessageAction(icon: LineAwesomeIcons.camera, onTap: () {}),
-        buildMessageAction(icon: LineAwesomeIcons.photo_video, onTap: () {}),
-        buildMessageAction(icon: LineAwesomeIcons.microphone, onTap: () {}),
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: 5),
-          child: Container(
-            width: MediaQuery
-                .of(context)
-                .size
-                .width * .58,
-            // height: 20,
-            child: TextField(
-              decoration: const InputDecoration(
-                hintText: "Convey your message",
-                border: InputBorder.none,
-              ),
-              onChanged: (String message) {
-                text = message;
-              },
-              autofocus: true,
-              autocorrect: true,
-              keyboardType: TextInputType.multiline,
-            ),
-          ),
-        ),
-        buildMessageAction(
-            icon: Icons.send,
-            onTap: () {
-              sendMessage(chatId, Message(text: text, senderId: currentUserId));
-            }),
-      ],
-    ),
-  );
-}
 
 class ChatActivity extends StatelessWidget {
   final String chatId;
@@ -135,6 +13,7 @@ class ChatActivity extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(),
         body: SafeArea(
           child: Container(
             child: Column(
@@ -148,15 +27,17 @@ class ChatActivity extends StatelessWidget {
                     builder: (BuildContext context,
                         AsyncSnapshot<Map<String, Chat>> data) {
                       if (data.hasData && data.data[chatId] != null) {
-                        return buildChatMessagesUI(
-                            context, data.data[chatId].messages);
+                        return ChatMessages(
+                          context: context,
+                          messages: data.data[chatId].messages,
+                        );
                       } else {
                         return Text("No Data");
                       }
                     },
                   ),
                 ),
-                buildMessageInputUI(context, chatId: chatId),
+                MessageInputUI(context: context, chatId: chatId),
               ],
             ),
           ),
