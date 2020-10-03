ae7957e7c087623b34614fe7b194a5d26d26ef76
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-10-03 21:51:36 +0530

    Added new designs

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:line_awesome_flutter/line_awesome_flutter.dart';
-import 'package:memomessenger/Activities/ChatsActivity/Activity.dart';
 import 'package:memomessenger/Services/Constants.dart';
 
 void main() {
   runApp(Main());
-}
-
-BottomNavigationBarItem getBottomNavigationItem(
-    {@required String name,
-    @required IconData inactiveIcon,
-    @required IconData activeIcon}) {
-  return BottomNavigationBarItem(
-    icon: Icon(inactiveIcon, color: bottomAppBarIconColor),
-    title: Text(name, style: bottomAppBarIconLabelTextStyle),
-    activeIcon: Icon(activeIcon, color: bottomAppBarIconColor),
-  );
 }
 
 class Main extends StatelessWidget {
@@ -36,9 +23,6 @@ class MemoMessenger extends StatefulWidget {
 }
 
 class _MemoMessengerState extends State<MemoMessenger> {
-  int _navCurrentIndex = 2;
-  PageController pageController = new PageController(initialPage: 2);
-
   void prepareData() async {}
 
   @override
@@ -51,57 +35,7 @@ class _MemoMessengerState extends State<MemoMessenger> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: PageView(
-          controller: pageController,
-          onPageChanged: (int index) {
-            setState(() {
-              this._navCurrentIndex = index;
-            });
-          },
-          children: [
-            Container(),
-            Container(
-              child: Text("Hello world!"),
-            ),
-            ChatsActivity(),
-            Container(),
-            Container(),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          getBottomNavigationItem(
-            name: "People",
-            inactiveIcon: Icons.people_outline,
-            activeIcon: Icons.people,
-          ),
-          getBottomNavigationItem(
-            name: "Labels",
-            inactiveIcon: Icons.label_outline,
-            activeIcon: Icons.label,
-          ),
-          getBottomNavigationItem(
-            name: "Chats",
-            inactiveIcon: Icons.chat_bubble_outline,
-            activeIcon: Icons.chat_bubble,
-          ),
-          getBottomNavigationItem(
-            name: "Call",
-            inactiveIcon: LineAwesomeIcons.phone,
-            activeIcon: Icons.phone,
-          ),
-          getBottomNavigationItem(
-            name: "Profile",
-            inactiveIcon: Icons.person_outline,
-            activeIcon: Icons.person,
-          ),
-        ],
-        currentIndex: this._navCurrentIndex,
-        elevation: 20,
-        onTap: (index) {
-          this.pageController.jumpToPage(index);
-        },
+        child: Text("Hello"),
       ),
     );
   }
